421c0caa31e12567f5896daa1cb9a4c7413121af
karthi742 <karthiazp@gmail.com>
2025-08-20 08:21:48 +0530

    changes database

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT,
-    database:process.env.DB_NAME,
+ host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  port: process.env.MYSQLPORT,
+  database: process.env.MYSQLDATABASE,
 })
 
 module.exports = pool
